9a19ee3d534a9c9e553392d46505d7cdae6948fe
Nikolaev Nikita <69678682+NikitkaN@users.noreply.github.com>
2026-07-28 16:22:37 +0300

    Add Vite, env. index.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// import * as path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -14,10 +14,4 @@ export default defineConfig({
       '/api': 'http://localhost:3001',
     },
   },
-//   resolve: {
-//     alias: {
-//       // Вместо '../../components/Button' можно писать '@/components/Button'
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-});
+})
